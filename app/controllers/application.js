1654b9c5bb802b7b333383193d29eb6e836bddc2
Greg Levine <levine.greg@gmail.com>
2014-11-11 22:50:22 -0500

    Clicking on the website title will take you home.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     actions: {
+        routeToHome: function() {
+            this.transitionToRoute('/');
+        },
+
         routeToRepo: function(owner, repo) {
             this.transitionToRoute('repo', owner, repo);
         }
